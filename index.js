37f66734e294d0dc65db04afd5f5b1aa85d751f0
“laurendoesntlikeusernames” <Lauren.langford.mail@gmail.com>
2022-05-30 21:19:45 +0800

    node-static

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const http = require('http')
-const fs = require('fs')
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-})
-
-server.listen(process.env.PORT || 3000)
+var static = require('node-static');
+ 
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./public');
+ 
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
